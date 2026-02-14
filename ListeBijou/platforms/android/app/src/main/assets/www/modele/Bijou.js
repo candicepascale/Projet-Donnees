38c582d66b9095d271a6bfa9500cc0e1b1dc853f
candicepascale <candice2ng@gmail.com>
2026-02-14 18:36:38 -0500

    update modele #1

--- ListeBijou/platforms/android/app/src/main/assets/www/modele/Bijou.js
+++ ListeBijou/platforms/android/app/src/main/assets/www/modele/Bijou.js
@@ -1,10 +1,9 @@
-class Bijou{
-    constructor(nom, type, prix, description, id){
-        this.id = id;
-        this.nom = nom;
-        this.type = type;
-        this.prix = prix;
-        this.description = description;
-    }
+class Bijou {
+  constructor(nom, type, prix, description, id) {
+    this.id = id; // string Firestore (ex: "aBcdE123...")
+    this.nom = nom;
+    this.type = type;
+    this.prix = prix;
+    this.description = description;
+  }
 }
-
